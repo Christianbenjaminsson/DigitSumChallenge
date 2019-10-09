79e76fdc7b9149c19f59ac71854bb59f6cb12ef8
Christian Benjaminsson <54481384+Christianbenjaminsson@users.noreply.github.com>
2019-10-09 12:08:33 +0200

    Push project to Github

---
+++ DigitSumChallenge.java
@@ -0,0 +1,29 @@
+package com.example.digitsumchallenge;
+
+public class DigitSumChallenge {
+    public static void main(String[] args) {
+        System.out.println("The sum of the digits in number 125 is " + sumDigits(125));
+        System.out.println("The sum of the digits in number -125 is " + sumDigits(-125));
+        System.out.println("The sum of the digits in number 4 is " + sumDigits(4));
+        System.out.println("The sum of the digits in number 32123 is " + sumDigits(32123));
+    }
+
+    private static int sumDigits(int number) {
+        if(number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+
+        while (number > 0) {
+            //Extract the least-significant digit
+            int digit = number % 10;
+            sum += digit;
+
+            //Drop the least-significant digit
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
